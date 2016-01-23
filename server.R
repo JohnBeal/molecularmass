e3755e4e52f20012eaf1d9c58a90beb9604e967d
JohnBeal <john_beal@paradise.net.nz>
2016-01-23 21:43:35 +1300

    Updated documentation, comments

--- server.R
+++ server.R
@@ -1,15 +1,35 @@
 
-## This function takes the user-inputed molecular formula and calculates the corresponding molecular mass,
+## This function takes the user-inputed molecular formula for a molecule containing any of the the elements 
+## carbon, hydrogen, nitrogen, oxygen and sulfur; and calculates the corresponding molecular mass,
 ## based on the atomic masses of the constituent atoms. 
 
 
 library(shiny)
 
-# Atomic masses (i.e. coefficients for predicting molecular mass)
+## Atomic masses (i.e. coefficients for predicting molecular mass)
 atomicmass <- c(12.01, 1.008, 14.01, 16.00, 32.07)
 names(atomicmass) <- c("C","H","N","O","S")
 
-shinyServer(function(input, output) {
 
+## Function takes input of number of carbon (C), hydrogen (H), nitrogen (N), oxygen (O) and sulfur (S) atoms in a molecule,
+## and returns the molecular mass. Function also requires vector of atomic masses.
+molecularmass <- function (nC, nH, nN, nO, nS, atomicmasses) {
+                        round(sum(nC*atomicmasses["C"],
+                                  nH*atomicmasses["H"],
+                                  nN*atomicmasses["N"],
+                                  nO*atomicmasses["O"],
+                                  nS*atomicmasses["S"]), 2)        
+}
+
+
+shinyServer(function(input, output) {
+        
+        ## Render the input number of atoms as a molecular formula using HTML 
+        output$molform <- renderText({paste("C", tags$sub(input$nC), "H", tags$sub(input$nH), "N", tags$sub(input$nN), 
+                                            "O", tags$sub(input$nO), "S", tags$sub(input$nS))})
+        
+        ## Calculate the molecular mass by calling the molecularmass function and passing the fucntion the input values
+        output$molmass <- renderText(molecularmass(nC = input$nC, nH = input$nH, nN = input$nN, nO = input$nO, nS = input$nS,
+                                                   atomicmass))
 
 })
--- ui.R
+++ ui.R
@@ -1,9 +1,7 @@
  
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
+# Calculating the molecular mass for a molecular formula representing the composition of a chemical compound
+# is an important operation is chemistry. This Shiny application allows a user to calculate the molecular mass for chemical 
+# compounds containing the elements carbon (C), hydrogen (H), nitrogen (N), oxygen (O) and sulfur (S).
 
 library(shiny)
 
@@ -12,8 +10,9 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Molecular Mass Calculator"),
-
-  # Sidebar with a slider input for number of bins
+        h4("Enter the number of carbon (C), hydrogen (H), oxygen (O), nitrogen (N) and sulfur (S) atoms in your 
+           compound to calculate the molecular mass."),        
+  # Sidebar with numeric inputs for number of C, H, N, O and S atoms in the molecule
   sidebarLayout(
     sidebarPanel(
       numericInput('nC', "C", 0, min = 0, step = 1),
@@ -22,7 +21,7 @@ shinyUI(fluidPage(
       numericInput('nO', "O", 0, min = 0, step = 1),
       numericInput('nS', "S", 0, min = 0, step = 1)),
 
-    # Show a plot of the generated distribution
+    # Display the molecular formula (return the user's input) and calculate the molecular mass
     mainPanel(
         h3("Your molecular formula:"),    
         htmlOutput("molform"),
