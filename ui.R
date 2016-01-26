0e9a856f3c3febbb01b828f3d0c697a22aea2695
unknown <j.beal@CISS28251.int.irl.cri.nz>
2016-01-27 10:50:17 +1300

    molecular mass calculator updated to include phosphorus

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
  
 # Calculating the molecular mass for a molecular formula representing the composition of a chemical compound
 # is an important operation is chemistry. This Shiny application allows a user to calculate the molecular mass for chemical 
-# compounds containing the elements carbon (C), hydrogen (H), nitrogen (N), oxygen (O) and sulfur (S).
+# compounds containing the elements carbon (C), hydrogen (H), nitrogen (N), oxygen (O), phosphorus (P) and sulfur (S).
 
 library(shiny)
 
@@ -10,15 +10,16 @@ shinyUI(fluidPage(
 
   # Application title
   titlePanel("Molecular Mass Calculator"),
-        h4("Enter the number of carbon (C), hydrogen (H), oxygen (O), nitrogen (N) and sulfur (S) atoms in your 
+        h4("Enter the number of carbon (C), hydrogen (H), oxygen (O), nitrogen (N), phosphorus (P) and sulfur (S) atoms in your 
            compound to calculate the molecular mass."),        
-  # Sidebar with numeric inputs for number of C, H, N, O and S atoms in the molecule
+  # Sidebar with numeric inputs for number of C, H, N, O, P and S atoms in the molecule
   sidebarLayout(
     sidebarPanel(
       numericInput('nC', "C", 0, min = 0, step = 1),
       numericInput('nH', "H", 0, min = 0, step = 1),
       numericInput('nN', "N", 0, min = 0, step = 1),
       numericInput('nO', "O", 0, min = 0, step = 1),
+      numericInput('nP', "P", 0, min = 0, step = 1),
       numericInput('nS', "S", 0, min = 0, step = 1)),
 
     # Display the molecular formula (return the user's input) and calculate the molecular mass
